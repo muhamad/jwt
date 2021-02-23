78a35068adc099eac59f4eae33622fdf2937ba8f
Alexander Batishchev <abatishchev@gmail.com>
2021-02-22 21:52:49 -1000

    Update RS512Algorithm.cs

--- src/JWT/Algorithms/RS512Algorithm.cs
+++ src/JWT/Algorithms/RS512Algorithm.cs
@@ -40,12 +40,6 @@ namespace JWT.Algorithms
         }
 
         /// <inheritdoc />
-        public override string Name => JwtAlgorithmName.RS512.ToString();
-
-#if NET35 || NET40
-        protected override string HashAlgorithmInternal => HashAlgorithmName.SHA512;
-#else
-        protected override HashAlgorithmName HashAlgorithmInternal => HashAlgorithmName.SHA512;
-#endif
+        public override HashAlgorithmName HashAlgorithmName => HashAlgorithmName.SHA512;
     }
 }
