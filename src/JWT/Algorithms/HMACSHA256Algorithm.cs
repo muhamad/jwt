72486c53ea3ed71623c03c68e9bbf2b18265d653
Alexander Batishchev <abatishchev@gmail.com>
2021-02-22 22:23:19 -1000

    Update HMACSHA256Algorithm.cs

--- src/JWT/Algorithms/HMACSHA256Algorithm.cs
+++ src/JWT/Algorithms/HMACSHA256Algorithm.cs
@@ -5,7 +5,7 @@ namespace JWT.Algorithms
     /// <summary>
     /// HMAC using SHA-256
     /// </summary>
-    public sealed class HMACSHA256Algorithm : HMACAlgorithm
+    public sealed class HMACSHA256Algorithm : HMACSHAAlgorithm
     {
         /// <inheritdoc />
         public byte[] Sign(byte[] key, byte[] bytesToSign)
